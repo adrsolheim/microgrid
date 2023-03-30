f6e10e68acfbf5faaae52bb387984cfc37bea187
adrsolheim <adrsolheim@gmail.com>
2023-03-30 16:34:27 +0200

    Logging comment service

--- comment-service/src/main/java/no/adrsolheim/controller/CommentController.java
+++ comment-service/src/main/java/no/adrsolheim/controller/CommentController.java
@@ -1,5 +1,6 @@
 package no.adrsolheim.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import no.adrsolheim.dto.CommentDTO;
 import no.adrsolheim.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +9,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@Slf4j
 @RestController
 @RequestMapping(value = "/api/comment")
 public class CommentController {
@@ -17,11 +19,13 @@ public class CommentController {
 
     @GetMapping("/{id}")
     public List<CommentDTO> getComments(@PathVariable("id") Long blogId) {
+        log.info("Processing GET request for blogId: {}..", blogId);
         return commentService.getBlogComments(blogId);
     }
 
     @GetMapping
     public List<CommentDTO> getAllComments() {
+        log.info("Received GET request to fetch all comments");
         return commentService.getAllBlogComments();
     }
 
@@ -30,8 +34,10 @@ public class CommentController {
     public String addComment(@RequestBody CommentDTO commentDTO) {
         boolean added = commentService.addComment(commentDTO);
         if (added) {
+            log.info("Comment successfully added to blog with id {}", commentDTO, commentDTO.getBlogId());
             return "Comment successfully posted!";
         }
+        log.info("Failed to add comment {} to blog with id {}", commentDTO, commentDTO.getBlogId());
         return "Failed to post comment";
     }
 
